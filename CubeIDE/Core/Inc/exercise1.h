9b0152fde24df7ef3260651bc181e0840cdc9056
Kha Tran <tuankhavntv@gmail.com>
2024-09-10 09:45:18 +0700

    complete Ex1

---
+++ CubeIDE/Core/Inc/exercise1.h
@@ -0,0 +1,20 @@
+/*
+ * exercise1.h
+ *
+ *  Created on: Sep 10, 2024
+ *      Author: tuank
+ */
+
+#ifndef INC_EXERCISE1_H_
+#define INC_EXERCISE1_H_
+
+#include"main.h"
+
+#define LED_ON 1
+#define LED_OFF 0
+
+void init_exercise1();
+void exercise1_run();
+
+
+#endif /* INC_EXERCISE1_H_ */
--- CubeIDE/Core/Src/main.c
+++ CubeIDE/Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include"exercise1.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -93,6 +93,9 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	 exercise1_run();
+
+	 HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
