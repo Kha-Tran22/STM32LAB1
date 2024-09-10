1179356e2b4175ba78694ef9cfb2f2a038d08709
Kha Tran <tuankhavntv@gmail.com>
2024-09-10 22:14:23 +0700

    no message

--- CubeIDE/Core/Src/exercise4.c
+++ CubeIDE/Core/Src/exercise4.c
@@ -5,6 +5,63 @@
  *      Author: tuank
  */
 
+
+#include "exercise4.h"
+
+int counter = 0;
+
+char seg[10] =
+{
+// Dung bang chan tri de xac dinh cac doan led nao sang, doan led nao tat
+// Vi su dung Led-segment Anode nen den sang se o muc luan li 0
+//   g  f  e  d  c  b  a
+//0  1  1  1  1  0  0  1	0x40
+//1  1  1  1  1  0  0  1	0x79
+//2  0  1  0  0  1  0  0	0x24
+//3  0  1  1  0  0  0  0	0x30
+//4  0  0  1  1  0  0  1 	0x19
+//5  0  0  1  0  0  1  0	0x12
+//6  0  0  0  0  0  1  0	0x02
+//7  1  1  1  1  0  0  0 	0x78
+//8  0  0  0  0  0  0  0 	0x00
+//9  0  0  1  0  0  0  0 	0x10
+
+		0x40,
+		0x79,
+		0x24,
+		0x30,
+		0x19,
+		0x12,
+		0x02,
+		0x78,
+		0x00,
+		0x10
+};
+
+void display_number(int number)
+{
+	char code = seg[number];
+
+	// Kiem tra bit tuong ung SEG cua so can hien thi neu = 1 thi SEG OFF
+	HAL_GPIO_WritePin(SEG_A_GPIO_Port, SEG_A_Pin, (code & 0x01) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(SEG_B_GPIO_Port, SEG_B_Pin, (code & 0x02) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(SEG_C_GPIO_Port, SEG_C_Pin, (code & 0x04) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(SEG_D_GPIO_Port, SEG_D_Pin, (code & 0x08) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(SEG_E_GPIO_Port, SEG_E_Pin, (code & 0x10) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(SEG_F_GPIO_Port, SEG_F_Pin, (code & 0x20) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+	HAL_GPIO_WritePin(SEG_G_GPIO_Port, SEG_G_Pin, (code & 0x40) ? GPIO_PIN_SET : GPIO_PIN_RESET);
+}
+
+void exercise4_run()
+{
+	display_number(counter++);
+	if (counter >= 10)
+	{
+		counter = 0;
+	}
+}
+
+
 //#include"exercise4.h"
 //
 //
