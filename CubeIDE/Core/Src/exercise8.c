4aeea3da9afd07ab5db15d7dfd7617a1afdda2e0
Kha Tran <tuankhavntv@gmail.com>
2024-09-11 12:49:02 +0700

    no message

--- CubeIDE/Core/Src/exercise8.c
+++ CubeIDE/Core/Src/exercise8.c
@@ -11,8 +11,7 @@
 int counter = 0;
 
 void init_exercise8(){}
-void clearAllClock();
-void setNumberOnClock(int num);
+
 
 
 uint16_t LED[12] =
@@ -32,25 +31,9 @@ uint16_t LED[12] =
 
 };
 
-void clearAllClock()
-{
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_4 , GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_5 , GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_6 , GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_7 , GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_8 , GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_9 , GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_10 , GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_11 , GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_12 , GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_13 , GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_14 , GPIO_PIN_SET);
-	HAL_GPIO_WritePin(GPIOA, GPIO_PIN_15 , GPIO_PIN_SET);
-}
-
 void setNumberOnClock(int num)
 {
-	clearAllClock();
+
 	HAL_GPIO_WritePin(GPIOA, LED[num], GPIO_PIN_RESET);
 }
 
